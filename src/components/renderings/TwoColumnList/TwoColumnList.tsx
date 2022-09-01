719b75d196a8fdd8ef2c14cd8ef1c8391b5a6d93
Rasmus Thirslund <rasmus.thirslund@ombrello.io>
2022-09-01 13:43:52 +0200

    conditionally display of grid debugger

--- src/components/renderings/TwoColumnList/TwoColumnList.tsx
+++ src/components/renderings/TwoColumnList/TwoColumnList.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 import { Link } from "@atoms";
 import { Col, Container, Row, Section } from "@layout";
 import { TwoColumnListProps } from "@types";
@@ -8,8 +10,49 @@ const TwoColumnList = ({ heading, imageCards = [] }: TwoColumnListProps) => {
   const list1 = new Array(...imageCards);
   const list2 = list1.splice(spliceAtIndex);
 
+  const containerRef = useRef(null);
+  const listOneRef = useRef(null);
+  const listTwoRef = useRef(null);
+
+  useEffect(() => {
+    const trigger = containerRef.current;
+
+    if (trigger) {
+      gsap.fromTo(
+        listOneRef.current,
+        {
+          y: 0,
+        },
+        {
+          y: -100,
+          scrollTrigger: {
+            trigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: 0.1,
+          },
+        }
+      );
+      gsap.fromTo(
+        listTwoRef.current,
+        {
+          y: 0,
+        },
+        {
+          y: -800,
+          scrollTrigger: {
+            trigger,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: 0.1,
+          },
+        }
+      );
+    }
+  }, []);
+
   return (
-    <Section rendering="TwoColumnList">
+    <Section rendering="TwoColumnList" flush className="-mb-[500px]">
       <Container className="max-w-page">
         <Row>
           <Col md="10" push={{ md: 1 }}>
@@ -18,67 +61,74 @@ const TwoColumnList = ({ heading, imageCards = [] }: TwoColumnListProps) => {
             )}
           </Col>
         </Row>
-        {imageCards && imageCards.length && (
-          <Row className="gap-y-lg">
-            <Col
-              sm="6"
-              md="5"
-              push={{ md: 1 }}
-              className="space-y-lg sm:space-y-0"
-            >
-              {list1?.map((cardItem, index) => {
-                return cardItem.internalLink &&
-                  cardItem.internalLink.page.slug ? (
-                  <Link
-                    key={`${cardItem.internalLink.page.slug}_${index}`}
-                    href={cardItem.internalLink.page.slug}
-                    className="mb-6"
-                    block
-                  >
-                    <ListItem
-                      {...cardItem}
-                      last={index === list1.length - 1}
-                      first={index === 0}
-                    />
-                  </Link>
-                ) : (
-                  <ListItem
-                    key={`${cardItem.imageCardTitle}_${index}`}
-                    {...cardItem}
-                    last={index === list1.length - 1}
-                    first={index === 0}
-                  />
-                );
-              })}
-            </Col>
-            <Col sm="6" md="5" className="space-y-lg sm:space-y-0">
-              {list2?.map((cardItem, index) => {
-                return cardItem.internalLink &&
-                  cardItem.internalLink.page.slug ? (
-                  <Link
-                    key={`${cardItem.internalLink.page.slug}_${index}`}
-                    href={cardItem.internalLink.page.slug}
-                    className="mb-6"
-                    block
-                  >
-                    <ListItem
-                      {...cardItem}
-                      topSpace={index === 0}
-                      last={index === list2.length - 1}
-                    />
-                  </Link>
-                ) : (
-                  <ListItem
-                    key={`${cardItem.imageCardTitle}_${index}`}
-                    {...cardItem}
-                    topSpace={index === 0}
-                    last={index === list2.length - 1}
-                  />
-                );
-              })}
-            </Col>
-          </Row>
-        )}
+        {/*  */}
+        <div className="relative" ref={containerRef}>
+          {imageCards && imageCards.length && (
+            <Row className="gap-y-lg">
+              <Col
+                sm="6"
+                md="5"
+                push={{ md: 1 }}
+                className="space-y-lg sm:space-y-0"
+              >
+                <div ref={listOneRef}>
+                  {list1?.map((cardItem, index) => {
+                    return cardItem.internalLink &&
+                      cardItem.internalLink.page.slug ? (
+                      <Link
+                        key={`${cardItem.internalLink.page.slug}_${index}`}
+                        href={cardItem.internalLink.page.slug}
+                        className="mb-6"
+                        block
+                      >
+                        <ListItem
+                          {...cardItem}
+                          last={index === list1.length - 1}
+                          first={index === 0}
+                        />
+                      </Link>
+                    ) : (
+                      <ListItem
+                        key={`${cardItem.imageCardTitle}_${index}`}
+                        {...cardItem}
+                        last={index === list1.length - 1}
+                        first={index === 0}
+                      />
+                    );
+                  })}
+                </div>
+              </Col>
+              <Col sm="6" md="5" className="space-y-lg sm:space-y-0">
+                <div ref={listTwoRef}>
+                  {list2?.map((cardItem, index) => {
+                    return cardItem.internalLink &&
+                      cardItem.internalLink.page.slug ? (
+                      <Link
+                        key={`${cardItem.internalLink.page.slug}_${index}`}
+                        href={cardItem.internalLink.page.slug}
+                        className="mb-6"
+                        block
+                      >
+                        <ListItem
+                          {...cardItem}
+                          topSpace={index === 0}
+                          last={index === list2.length - 1}
+                        />
+                      </Link>
+                    ) : (
+                      <ListItem
+                        key={`${cardItem.imageCardTitle}_${index}`}
+                        {...cardItem}
+                        topSpace={index === 0}
+                        last={index === list2.length - 1}
+                      />
+                    );
+                  })}
+                </div>
+              </Col>
+            </Row>
+          )}
+        </div>
       </Container>
     </Section>
   );
