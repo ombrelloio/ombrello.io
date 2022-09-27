65fded51214c16d07f4a99f6df0c354501af952c
Jacob Frederiksen <jacobfred88@gmail.com>
2022-09-27 14:57:43 +0200

    fisk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -59,19 +59,21 @@ function App({ Component, pageProps }: AppProps) {
           crossOrigin="crossOrigin"
         />
       </Head>
+
       <Script
-        id="clarity"
-        strategy="beforeInteractive"
+        id="Clarty"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "du5dg3g8uw");
-          `,
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "du5dg3g8uw");
+  `,
         }}
       />
+
       <Component {...pageProps} />
     </>
   );
