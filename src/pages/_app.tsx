6093d934796888b94a6682f549a1571f6c0ebb92
Jacob Frederiksen <jacobfred88@gmail.com>
2022-10-04 20:46:33 +0200

    add tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -74,6 +74,28 @@ function App({ Component, pageProps }: AppProps) {
         }}
       />
 
+      <Script
+        id="matomo"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          
+      var _paq = window._paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="https://ombrelloiovercelapp.matomo.cloud/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.async=true; g.src='//cdn.matomo.cloud/ombrelloiovercelapp.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+      })();
+    
+  `,
+        }}
+      />
+
       <Component {...pageProps} />
     </>
   );
